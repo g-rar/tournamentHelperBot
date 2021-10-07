9d45ee31c6de7036fe73ed4610e9f00dbeb12725
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2021-10-07 17:35:23 -0600

    ✨ Added get_reactions command

--- commands/server.py
+++ commands/server.py
@@ -1,12 +1,23 @@
+import asyncio
+import pandas as pd
+from io import StringIO
+from datetime import datetime
 from controllers.admin import adminCommand
 from controllers.server import Server, serverController
 
 import strings as strs
 
+import discord
+from discord.file import File
+from discord.message import Message
+from discord.user import User
+from discord.channel import TextChannel
+from discord_slash.utils.manage_commands import create_option
 from discord_slash.context import SlashContext
 
-from bot import botGuilds, slash
+from bot import botGuilds, slash, bot
 from devCommands.devCommands import devCommand
+from utils import OptionTypes
 
 
 
@@ -30,4 +41,50 @@ async def slashRegisterServer(ctx:SlashContext):
     else:
         await ctx.send(strs.SpanishStrs.DB_UPLOAD_ERROR)
 
-
+@slash.slash(
+    name="get_reactions",
+    description="Get the discord tags of the users who reacted to a message.",
+    guild_ids = botGuilds,
+    options= [
+        create_option(
+            name="channel", description="Channel in which the message is in.",
+            option_type=OptionTypes.CHANNEL, required= True
+        ),
+        create_option(
+            name="message_id", description="Message id at which users reacted for check-in, (you can get this by right clicking the message)",
+            option_type=OptionTypes.STRING, required=True
+        ),
+    ]
+)
+async def getReactions(ctx:SlashContext, channel:TextChannel, message_id:str):
+    if channel.type != discord.ChannelType.text:
+        await ctx.send(strs.SpanishStrs.VALUE_SHOULD_BE_TEXT_CHANNEL.format(option="channel"))
+        return
+    if ctx.guild_id is None:
+        await ctx.send(strs.SpanishStrs.NOT_FOR_DM)
+        return
+    messageId = int(message_id)
+    try:
+        msg:Message = await channel.fetch_message(messageId)
+    except Exception as e:
+        await ctx.send(strs.SpanishStrs.MESSAGE_NOT_FOUND.format(data =type(e).__name__))
+        return
+    
+    def check(_, user):
+        return user == ctx.author
+    try:
+        res1 = await ctx.send(strs.SpanishStrs.INPUT_CHECK_IN_REACTION)
+        inputReaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send('👎')
+        return
+    reaction = list(filter(lambda x: x.emoji == inputReaction.emoji, msg.reactions))
+    if reaction == []:
+        await ctx.send(strs.SpanishStrs.NO_REACTION_IN_MSG.format(reaction=str(inputReaction.emoji)))
+        return
+    reaction = reaction[0]
+    participants = []
+    async for user in reaction.users():
+        participants.append(f"{user.display_name}#{user.discriminator}")
+    df = pd.DataFrame(participants)    
+    await ctx.send(file=File(StringIO(df.to_csv()), filename= f"Participants_{datetime.utcnow()}.txt"))
