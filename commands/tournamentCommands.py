394169481ba97db753da9cc2bd588f1aacd9970f
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2022-01-22 16:06:57 -0600

    🐞Dont remove roles on participant refresh when update false

--- commands/tournamentCommands.py
+++ commands/tournamentCommands.py
@@ -370,15 +370,15 @@ async def refreshParticipants(ctx:CustomContext, tournament:str, update:bool = F
         participantController.updateParticipants(newParticipants)
         participantController.deleteParticipants([p for p,_ in failed])
     if len(failed) != 0:
+        await ctx.channel.send(file=File(StringIO(failedViews.to_csv()), filename= f"Disqualified_{datetime.utcnow()}.csv"))
         if update:
             await ctx.sendLocalized(StringsNames.PARTICIPANTS_DELETED, _as_reply=False, amount=len(failed))
-        await ctx.channel.send(file=File(StringIO(failedViews.to_csv()), filename= f"Disqualified_{datetime.utcnow()}.csv"))
-        if tournamentObj.registration.participantRole:
-            role:discord.Role = ctx.guild.get_role(tournamentObj.registration.participantRole)
-            for p,r in failed:
-                member:Member = ctx.guild.get_member(p.discordId)
-                await member.remove_roles(role, reason=f"Disquialified for: {r}")
-            await ctx.sendLocalized(StringsNames.PARTICIPANTS_ROLE_REMOVED, _as_reply=False, rolename=role.name)
+            if tournamentObj.registration.participantRole:
+                role:discord.Role = ctx.guild.get_role(tournamentObj.registration.participantRole)
+                for p,r in failed:
+                    member:Member = ctx.guild.get_member(p.discordId)
+                    await member.remove_roles(role, reason=f"Disquialified for: {r}")
+                await ctx.sendLocalized(StringsNames.PARTICIPANTS_ROLE_REMOVED, _as_reply=False, rolename=role.name)
 
 
 @slash.subcommand(
